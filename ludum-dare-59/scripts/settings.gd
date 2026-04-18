206ca3d2ce0919ca9b74e6c2591ea17ebf6a1ce0
Jijasan <jijek@jij.jij>
2026-04-18 18:09:11 +0300

    Add a resolution settings

--- ludum-dare-59/scripts/settings.gd
+++ ludum-dare-59/scripts/settings.gd
@@ -5,15 +5,27 @@ func _ready() -> void:
 	$VBoxContainer/FullHDButton.pressed.connect(_on_fullhd_pressed)
 	$VBoxContainer/FourKButton.pressed.connect(_on_4k_pressed)
 	$VBoxContainer/BackButton.pressed.connect(_on_back_pressed)
+	_update_buttons()
+	ResolutionManager.resolution_changed.connect(_on_resolution_changed)
 
 
 func _on_fullhd_pressed() -> void:
-	pass
+	ResolutionManager.set_resolution(ResolutionManager.Resolution.FULL_HD)
 
 
 func _on_4k_pressed() -> void:
-	pass
+	ResolutionManager.set_resolution(ResolutionManager.Resolution.FOUR_K)
 
 
 func _on_back_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/menu.tscn")
+
+
+func _on_resolution_changed(_res: ResolutionManager.Resolution) -> void:
+	_update_buttons()
+
+
+func _update_buttons() -> void:
+	var is_4k := ResolutionManager.current == ResolutionManager.Resolution.FOUR_K
+	$VBoxContainer/FullHDButton.disabled = !is_4k
+	$VBoxContainer/FourKButton.disabled = is_4k
