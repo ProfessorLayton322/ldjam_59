e9433c13a693ff7028a3c861ebcdbed77358bc06
Stasana <stasana@stasana.local>
2026-04-18 17:38:34 +0300

    add base tiles classes

---
+++ ludum-dare-59/scripts/tiles/spawner_tile.gd
@@ -0,0 +1,10 @@
+class_name SpawnerTile
+extends BaseTile
+
+
+func OnTrigger(source: Node = null) -> void:
+	super.OnTrigger(source)
+
+
+func OnEnter(source: Node = null) -> void:
+	super.OnEnter(source)
